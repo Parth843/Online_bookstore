9fe58c32cc3fbd04f974a11a2cc0b079fd39bf47
Parth843 <parthkurane10@gmail.com>
2021-05-21 14:31:13 +0530

    ch13

--- books/tests.py
+++ books/tests.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
 from django.test import TestCase
 from django.urls import reverse
 from .models import Book, Review
@@ -12,6 +13,10 @@ class BookTests(TestCase):
             email = 'something@asemail.com',
             password = 'testpass123'
         )
+        
+        self.special_permission = Permission.objects.get(
+            codename = 'special_status')
+
 
         self.book = Book.objects.create(
             title = 'Harry Potter',
@@ -29,15 +34,30 @@ class BookTests(TestCase):
         self.assertEqual(f'{self.book.author}', 'J.K.Rowling')
         self.assertEqual(f'{self.book.price}', '3000')
 
-    def test_book_list_view(self):
+    def test_book_list_view_for_logged_in_user(self):
+        self.client.login(email='something@asemail.com', password='testpass123')
         resp = self.client.get(reverse('book_list'))
         self.assertEqual(resp.status_code, 200)
         self.assertContains(resp, 'Harry Potter')
         self.assertTemplateUsed(resp, 'books/book_list.html')
 
-    def test_book_detail_view(self):
+    def test_booklist_view_for_logged_out_user(self):
+        self.client.logout()
+        resp = self.client.get(reverse('book_list'))
+        self.assertEqual(resp.status_code, 302)
+        self.assertRedirects(
+            resp, '%s?next=/books/'%(reverse('account_login'))
+        )
+        resp = self.client.get(
+            '%s?next=/books/'%(reverse('account_login'))
+        )
+        self.assertContains(resp, "Log In")
+
+    def test_book_detail_view_with_permissions(self):
+        self.client.login(email='something@asemail.com', password='testpass123')
+        self.user.user_permissions.add(self.special_permission)
         resp = self.client.get(self.book.get_absolute_url())
-        no_resp = self.client.get('/books/12345')
+        no_resp = self.client.get('/boooks/123')
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(no_resp.status_code, 404)
         self.assertContains(resp, 'Harry Potter')
--- config/settings.py
+++ config/settings.py
@@ -163,3 +163,6 @@ ACCOUNT_UNIQUE_EMAIL =  True
 
 DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
+
